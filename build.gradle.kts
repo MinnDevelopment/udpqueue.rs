2b10d1f1104904188555f818b84c2cb167be2296
Florian Spieß <business@minnced.club>
2023-04-19 14:50:05 +0200

    Add musl targets

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,22 +48,27 @@ subprojects {
     }
 
     group = "club.minnced"
-    version = "0.2.6"
+    version = "0.2.7"
 
     // See https://github.com/sedmelluq/lavaplayer/blob/master/common/src/main/java/com/sedmelluq/lava/common/natives/architecture/DefaultArchitectureTypes.java
     // identifier is the suffix used after the system name
     fun getPlatform(triplet: String) = when {
-        triplet.startsWith("x86_64")  && "linux"   in triplet -> "linux-x86-64"
-        triplet.startsWith("i686")    && "linux"   in triplet -> "linux-x86"
-        triplet.startsWith("aarch64") && "linux"   in triplet -> "linux-aarch64"
-        triplet.startsWith("arm")     && "linux"   in triplet -> "linux-arm"
+        triplet.startsWith("x86_64")  && "linux"   in triplet && "musl" in triplet -> "linux-musl-x86-64"
+        triplet.startsWith("i686")    && "linux"   in triplet && "musl" in triplet -> "linux-musl-x86"
+        triplet.startsWith("aarch64") && "linux"   in triplet && "musl" in triplet -> "linux-musl-aarch64"
+        triplet.startsWith("arm")     && "linux"   in triplet && "musl" in triplet -> "linux-musl-arm"
 
-        triplet.startsWith("x86_64")  && "windows" in triplet -> "win-x86-64"
-        triplet.startsWith("i686")    && "windows" in triplet -> "win-x86"
-        triplet.startsWith("aarch64") && "windows" in triplet -> "win-aarch64"
-        triplet.startsWith("arm")     && "windows" in triplet -> "win-arm"
+        triplet.startsWith("x86_64")  && "linux"   in triplet                      -> "linux-x86-64"
+        triplet.startsWith("i686")    && "linux"   in triplet                      -> "linux-x86"
+        triplet.startsWith("aarch64") && "linux"   in triplet                      -> "linux-aarch64"
+        triplet.startsWith("arm")     && "linux"   in triplet                      -> "linux-arm"
 
-        "darwin"  in triplet -> "darwin"
+        triplet.startsWith("x86_64")  && "windows" in triplet                      -> "win-x86-64"
+        triplet.startsWith("i686")    && "windows" in triplet                      -> "win-x86"
+        triplet.startsWith("aarch64") && "windows" in triplet                      -> "win-aarch64"
+        triplet.startsWith("arm")     && "windows" in triplet                      -> "win-arm"
+
+        "darwin" in triplet                                                        -> "darwin"
 
         else -> throw IllegalArgumentException("Unknown platform: $triplet")
     }

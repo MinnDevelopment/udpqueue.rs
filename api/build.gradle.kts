8187c7bfd31c983c1dfb347ead56fc62f000a046
Florian Spieß <business@minn.dev>
2025-12-23 12:47:41 +0100

    Update gradle dependencies

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -12,9 +12,9 @@ repositories {
 }
 
 dependencies {
-    implementation("org.slf4j:slf4j-api:1.7.25")
-    implementation("dev.arbjerg:lava-common:1.5.4")
-    compileOnly("net.dv8tion:JDA:5.0.2")
+    implementation("org.slf4j:slf4j-api:2.0.17")
+    implementation("dev.arbjerg:lava-common:1.5.6")
+    compileOnly("net.dv8tion:JDA:6.2.0")
 }
 
 val javadoc: Javadoc by tasks
